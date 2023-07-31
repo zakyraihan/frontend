66ee36d114cb2482273896ecb56db91255d73531
Ziad Syahrul <zakyraihan6336@email.com>
2023-07-31 14:00:09 +0700

    belajar props

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -3,16 +3,16 @@ import React from 'react'
 
 interface Container {
     nama: string
-    Card: ReactNode
+    children: ReactNode
 }
 
-const Card: React.FC<Container> = ({  nama, Card  }) => {
+const Card: React.FC<Container> = ({  nama, children  }) => {
   return (
     <div className="border-2">
         <div className="border-2 border-red-500">
             <h5 className="text-xl">{nama}</h5>
         </div>
-        <div className="py-3">{Card}</div>
+        <div className="py-3">{children}</div>
     </div>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -6,13 +6,13 @@ import Card from "./components/Card";
 const Home = () => {
 
   return (
-    <div className="container">
+    <div>
     <Latihan name="muhammad zaky raihan" userName="zaky" age={17} isVerified={true} />
     <Latihan name="muhammd reza" userName="reza" age={20} isVerified={false} />
     <Latihan name="shirin kirani faisal" userName="shirin" age={15} isVerified={true} />
-    {/* <Card nama="Ini Card">
+    <Card nama="Ini Card">
       <p>saya belajar children</p>
-    </Card> */}
+    </Card>
     <Button title="simpan" isDisabled={false}/>
     <Button title="cancel" isDisabled={true}/>
     </div>
