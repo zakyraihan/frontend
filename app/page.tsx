54c8e013b5ee0f7632597bfca47861c7943f145d
Ziad Syahrul <zakyraihan6336@email.com>
2023-08-04 09:36:55 +0700

    edit

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 import React from "react";
-import Latihan from "./components/Latihan";
-import Button from "./components/Button";
-import Card from "./components/Card";
+
 
 const Home = () => {
   return (
