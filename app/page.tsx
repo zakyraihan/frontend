c416f826ea3bcd0204235533fa67cd565c979bb7
Ziad Syahrul <zakyraihan6336@email.com>
2023-08-04 09:36:09 +0700

    edit

--- app/page.tsx
+++ app/page.tsx
@@ -6,15 +6,7 @@ import Card from "./components/Card";
 const Home = () => {
   return (
     <div>
-    <Latihan name="muhammd reza" userName="reza" age={20} isVerified={false} />
-    <Latihan name="shirin kirani faisal" userName="shirin" age={15} isVerified={true} />
-    <Button title="simpan" isDisabled={false}/>
-    <Button title="cancel" isDisabled={true}/>
-    <Card title="sayembara" subtitle="dikota magelang" >
-      <div className="p-3">
-       <Latihan name="muhammad zaky raihan" userName="zaky" age={17} isVerified={true} />
-       </div>
-    </Card>
+    
     </div>
   );
 }
