30af9171da4f9d5b795429edfb61adaa82cbb989
Ziad Syahrul <zakyraihan6336@email.com>
2023-08-07 09:41:21 +0700

    edit

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -3,9 +3,9 @@ interface ButtonProps {
     isDisabled?: boolean;
   }
   
-  const Button: React.FC<ButtonProps> = ({ title, isDisabled }) => {
+  const Button: React.FC<ButtonProps> = ({ title, isDisabled, }) => {
     return (
-      <button disabled={isDisabled} className="w-16 h-8 rounded border bg-red-400">{title}</button>
+      <button disabled={isDisabled} className="w-16 h-8 rounded border bg-red-400" >{title}</button>
     );
   };
   
